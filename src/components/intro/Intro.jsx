0698cbd8ac9a12a3d8a5ed13d10fd9896e88f85a
unknown <tkftkfdl970@naver.com>
2022-11-09 13:08:45 +0900

    프로필 사이트 구현 연습

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+import './intro.scss';
+import { init } from 'ityped';
+
+export default function Intro () {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer"],
+        });
+    },[]);
+
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>LEE YUN HEE</h1>
+                    <h3>Frontend <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
+    )
+}
